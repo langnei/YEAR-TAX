9507a99e2ed2be21d6bda6acee8133c0d4e3fece
Yoi <Yoi@DESKTOP-DGQV9JN>
2018-08-07 14:34:42 +0900

    commit

---
+++ egovtax/src/main/java/egovframework/tax/ksys/service/SystemColumnVO.java
@@ -0,0 +1,84 @@
+package egovframework.tax.ksys.service;
+
+public class SystemColumnVO {
+	
+	private String depaCode;
+	
+	private String emplName;
+	
+	private String logiName;
+	
+	private String passName;
+	
+	private String holdPres;
+	
+	private String inpuMan;
+	
+	private String inpuDate;
+	
+	private String depaName;
+	
+	public String getDepaCode() {
+		return depaCode;
+	}
+
+	public void setDepaCode(String depaCode) {
+		this.depaCode = depaCode;
+	}
+
+	public String getEmplName() {
+		return emplName;
+	}
+
+	public void setEmplName(String emplName) {
+		this.emplName = emplName;
+	}
+
+	public String getLogiName() {
+		return logiName;
+	}
+
+	public void setLogiName(String logiName) {
+		this.logiName = logiName;
+	}
+
+	public String getPassName() {
+		return passName;
+	}
+
+	public void setPassName(String passName) {
+		this.passName = passName;
+	}
+
+	public String getHoldPres() {
+		return holdPres;
+	}
+
+	public void setHoldPres(String holdPres) {
+		this.holdPres = holdPres;
+	}
+
+	public String getInpuMan() {
+		return inpuMan;
+	}
+
+	public void setInpuMan(String inpuMan) {
+		this.inpuMan = inpuMan;
+	}
+
+	public String getInpuDate() {
+		return inpuDate;
+	}
+
+	public void setInpuDate(String inpuDate) {
+		this.inpuDate = inpuDate;
+	}
+
+	public String getDepaName() {
+		return depaName;
+	}
+
+	public void setDepaName(String depaName) {
+		this.depaName = depaName;
+	}	
+}
